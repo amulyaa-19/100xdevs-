0d3667052ffdcf5a4cc3948d47ae9a0706c2d1d5
Amulya Srivastava <146869340+amulyaa-19@users.noreply.github.com>
2024-09-10 15:33:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express") ;
 const jwt = require('jsonwebtoken');
+const path = require('path')
 const JWT_SECRET = "randomText";
 const app = express();
 
 const users = [];
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'frontend'))); 
 
 // should return a random long string
 
@@ -14,9 +16,9 @@ function logger(req , res ,next){
     next();
 }
 
-app.get("/" , function(req , res){
-    res.sendFile(__dirname + "frontend/index.html");
-})
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
 
 app.post("/signup", logger , (req, res) => {
   const username = req.body.username;
@@ -55,40 +57,39 @@ app.post("/signin", logger , (req, res) => {
   }
 });
 
-function auth(req , res ,next){
+function auth(req, res, next) {
     const token = req.headers.token;
-    const decodedData = jwt.verify(token , JWT_SECRET);
-    if(decodedData.username){
-        req.username = decodedData.username;
-        next()
-    }else{
-        res.json({
-        message: "You are not logged in"
-        })
+    try {
+        const decodedData = jwt.verify(token, JWT_SECRET);
+        if (decodedData.username) {
+            req.username = decodedData.username;
+            next();
+        } else {
+            res.status(401).json({
+                message: "You are not logged in"
+            });
+        }
+    } catch (error) {
+        res.status(401).json({
+            message: "Invalid or expired token"
+        });
     }
 }
 
+app.get("/me", logger, auth, function (req, res) {
+    const foundUser = users.find(user => user.username === req.username);
 
-
-app.get("/me",logger , auth ,function (req, res) {
-    const token = req.headers.authorization;
-    const userDetails = jwt.verify(token, JWT_SECRET);//used to verify i can decode it
-
-
-
-    if(decodedData.username){
-      let foundUser = null;
-
-      for(let i = 0 ;i<users.length ; i++){
-        if(users[i].username === req.username){
-            foundUser = users[i];
-        }
-      }
-      res.json({
-        username: foundUser.username,
-        password: foundUser.password
-      })
+    if (foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        });
+    } else {
+        res.status(404).json({
+            message: "User not found"
+        });
     }
-    
-})
-app.listen(3009)
+});
+app.listen(3009, () => {
+    console.log("Server is running on http://localhost:3009");
+});
